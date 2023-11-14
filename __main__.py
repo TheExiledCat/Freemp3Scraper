255290611c86c261589207b42d3275ce8e3635a7
TheExiledCat <kazekage2002@hotmail.com>
2023-11-14 01:45:00 +0100

    Added selenium

--- __main__.py
+++ __main__.py
@@ -1,1 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import sys
+import time
+driver  = webdriver.Chrome()
+driver.get("https://free-mp3-download.net")
+print("Test")
+if __name__=="__main__":
+    if len(sys.argv)>1:
+        print(sys.argv[1])
+m= input("ABC")
+time.sleep(10)
