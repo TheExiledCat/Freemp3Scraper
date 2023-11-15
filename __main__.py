b976b923eab3414dca237492233eb41d54e47856
Armando-codecafe <armando.roerdinkveldboom@code-cafe.nl>
2023-11-15 10:24:59 +0100

    Added config file, added options

--- __main__.py
+++ __main__.py
@@ -1,12 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import scraper 
 import sys
 import time
+import json
+import argparse
+import os
+parser = argparse.ArgumentParser()
+
+user = os.path.expanduser("~")
 driver  = webdriver.Chrome()
-driver.get("https://free-mp3-download.net")
-print("Test")
+
+parser.add_argument("-mode","--mode",help="Select download mode- options: album | track ")
+parser.add_argument("-p","--path",help="Select download path- default = "+user+"/Music")
+with open("config.json") as file:
+    options = json.load(file)
+
+print(options)
 if __name__=="__main__":
     if len(sys.argv)>1:
         print(sys.argv[1])
-m= input("ABC")
-time.sleep(10)
+        scraper.run(driver,options)
---
+++ scraper.py
@@ -0,0 +1,21 @@
+
+
+
+
+site = "https://free-mp3-download.net"
+
+
+def run(browser,scraper_options):
+    
+    match scraper_options["mode"]:
+        case "album": 
+            print("album mode")
+        
+    
+    input()
+    
+
+def scrape_album():
+    pass
+def scrape_track():
+    pass
