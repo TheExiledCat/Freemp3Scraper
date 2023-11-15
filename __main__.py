bc90b30b3a8b9d5b8dfda052f98bd23f64658b4d
Armando-codecafe <armando.roerdinkveldboom@code-cafe.nl>
2023-11-15 14:21:41 +0100

    fixed merge

--- __main__.py
+++ __main__.py
@@ -1,23 +1,49 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import scraper 
+import scraper as scraper 
 import sys
 import time
 import json
 import argparse
 import os
+import chromedriver_autoinstaller_fix
+import undetected_chromedriver as uWebDriver
+
+chromedriver_autoinstaller_fix.install()  
 parser = argparse.ArgumentParser()
 
-user = os.path.expanduser("~")
-driver  = webdriver.Chrome()
 
-parser.add_argument("-mode","--mode",help="Select download mode- options: album | track ")
-parser.add_argument("-p","--path",help="Select download path- default = "+user+"/Music")
+user = os.path.expanduser("~")
+
+
+parser.add_argument("-mode","--mode",help="Select download mode- options: album | track ",default="track")
+parser.add_argument("-p","--path",help="Select download path- default = "+user+"/Music/FreeMp3Parser",default=user+"/Music/FreeMp3Parser")
+parser.add_argument("-t","--title",help="Title: artist and album/track name")
+
+args = parser.parse_args()
+#print(f"{args.mode} {args.path}")
+chrome_options = uWebDriver.ChromeOptions()
+prefs = {"profile.default_content_settings.popups": 0,
+             "download.default_directory": args.path,#IMPORTANT - ENDING SLASH V IMPORTANT
+             "directory_upgrade": True}
+chrome_options.add_experimental_option('prefs',prefs)
+adblocker_path = "adblocker"
+chrome_options.add_argument('load-extension=' + adblocker_path)
+
+driver  = webdriver.Chrome(options=chrome_options)
+title = args.title
 with open("config.json") as file:
     options = json.load(file)
 
-print(options)
 if __name__=="__main__":
     if len(sys.argv)>1:
-        print(sys.argv[1])
-        scraper.run(driver,options)
+        print("Welcome to freemp3scrape, use the -h flag to find out all the commands")
+        print(driver.window_handles)
+        print("loading...")
+        while len(driver.window_handles)<2:
+            continue
+        for i in range(2):
+            driver.switch_to.window(driver.window_handles[1])
+            driver.close()
+        driver.switch_to.window(driver.window_handles[0])
+        scraper.run(driver,options["scraper_options"],title)
--- scraper.py
+++ scraper.py
@@ -1,21 +1,61 @@
-
-
-
-
+from selenium import webdriver
+import undetected_chromedriver as uWebDriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+import time
+from urllib.request import urlretrieve
 site = "https://free-mp3-download.net"
 
 
-def run(browser,scraper_options):
+def run(browser,scraper_options, title):
     
-    match scraper_options["mode"]:
-        case "album": 
-            print("album mode")
-        
+    if scraper_options["mode"]=="album":
+        scrape_album(browser,scraper_options,title)
+    elif scraper_options["mode"]=="track":
+        scrape_track(browser,scraper_options,title)
+    else:
+        print("error")
     
     input()
     
 
-def scrape_album():
-    pass
-def scrape_track():
+def scrape_album(browser:webdriver.Chrome,scraper_options,title):
+    
+   
+    browser.get(site)
+    search_form = browser.find_element(By.ID,"search")
+    input_box = search_form.find_element(By.XPATH,"//input")
+    input_box.send_keys(title)
+    search_button = search_form.find_element(By.XPATH,"//button")
+    search_button.click()
+    
+    table =WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.ID, "results_t")))
+
+    track_elements =table.find_elements(By.XPATH,"*")
+    #open pages
+
+    for track in track_elements:
+        print(track)
+        
+        
+        download_button = track.find_element(By.CLASS_NAME,"btn")
+        browser.execute_script("arguments[0].setAttribute('target',arguments[1])",download_button.find_element(By.XPATH,".."),"_blank")
+        download_button.click()
+        browser.switch_to.window(browser.window_handles[1])
+        
+        
+        
+        
+        
+        #browser.close()
+        browser.switch_to.window(browser.window_handles[0])
+    input()
+        
+    
+    
+def scrape_track(browser,scraper_options,title):
+    browser.get(site)
     pass
